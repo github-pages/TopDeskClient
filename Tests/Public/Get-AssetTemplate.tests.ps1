11da22dde23870d4e93e5d3d51463501956d4220
Ray Bury <ray@raybury.com>
2019-03-24 15:37:26 -0400

    Add new Get-AssetTemplate with tests

--- Tests/Public/Get-AssetTemplate.tests.ps1
+++ Tests/Public/Get-AssetTemplate.tests.ps1
@@ -1,5 +1,61 @@
-Describe Get-AssetTemplate {
-    It "Needs to have real tests" {
-        $true | Should be $true
+function Get-AssetTemplate
+{
+  <#
+    .Synopsis
+
+      Short description
+
+    .DESCRIPTION
+
+      Long description
+
+    .PARAMETER <Parameter-Name>
+
+      The description of a parameter.
+
+    .EXAMPLE
+
+      Example of how to use this cmdlet
+
+    .INPUTS
+
+      The Microsoft .NET Framework types of objects that can be piped to the function or script.
+      You can also include a description of the input objects.
+
+    .OUTPUTS
+
+      The .NET Framework type of the objects that the cmdlet returns.
+      You can also include a description of the returned objects.
+
+    .NOTES
+
+      Additional information about the function or script.
+
+    .LINK
+    
+      The name of a related topic. The value appears on the line below the ".LINK" keyword and must be preceded by a comment symbol # or included in the comment block.
+      Repeat the ".LINK" keyword for each related topic.
+  #>
+    [CmdletBinding(DefaultParameterSetName='Default',
+                SupportsShouldProcess=$true,
+                PositionalBinding=$false,
+                HelpUri = 'https://github.com/rbury/',
+                ConfirmImpact='Medium')]
+    [OutputType([String], ParameterSetName = "Default")]
+    Param
+    (
+
+    )
+    begin
+    {
+
+    }
+    process
+    {
+
+    }
+    end
+    {
+
     }
 }
--- TopDeskClient/Public/Get-AssetTemplate.ps1
+++ TopDeskClient/Public/Get-AssetTemplate.ps1
@@ -1,93 +1,68 @@
-﻿function Get-AssetTemplate {
-    <#
-  .Synopsis
+function Get-AssetTemplate
+{
+  <#
+    .Synopsis
 
-    Short description
+      Short description
 
-  .DESCRIPTION
+    .DESCRIPTION
 
-    Long description
+      Long description
 
-  .PARAMETER <Parameter-Name>
+    .PARAMETER <Parameter-Name>
 
-    The description of a parameter.
+      The description of a parameter.
 
-  .EXAMPLE
+    .EXAMPLE
 
-    Example of how to use this cmdlet
+      Example of how to use this cmdlet
 
-  .INPUTS
+    .INPUTS
 
-    The Microsoft .NET Framework types of objects that can be piped to the function or script.
-    You can also include a description of the input objects.
+      The Microsoft .NET Framework types of objects that can be piped to the function or script.
+      You can also include a description of the input objects.
 
-  .OUTPUTS
+    .OUTPUTS
 
-    The .NET Framework type of the objects that the cmdlet returns.
-    You can also include a description of the returned objects.
+      The .NET Framework type of the objects that the cmdlet returns.
+      You can also include a description of the returned objects.
 
-  .NOTES
+    .NOTES
 
-    Additional information about the function or script.
+      Additional information about the function or script.
 
-  .LINK
-
-    [Get-AssetTemplate](https://github.com/rbury/TopDeskClient/Docs/Get-AssetTemplate.md)
-
+    .LINK
+    
+      The name of a related topic. The value appears on the line below the ".LINK" keyword and must be preceded by a comment symbol # or included in the comment block.
+      Repeat the ".LINK" keyword for each related topic.
   #>
-    [CmdletBinding(DefaultParameterSetName = 'Default',
-        PositionalBinding = $false,
-        HelpUri = 'https://github.com/rbury/TopDeskClient/Docs/Get-AssetTemplate.md',
-        ConfirmImpact = 'Medium')]
+    [CmdletBinding(DefaultParameterSetName='Default',
+                SupportsShouldProcess=$true,
+                PositionalBinding=$false,
+                HelpUri = 'https://github.com/rbury/Docs/Get-AssetTemplate.md',
+                ConfirmImpact='Medium')]
+    [OutputType([PSObject])]
 
     Param ()
 
     begin {
-
-        if (!($script:tdConnected)) {
+        
+        if (-not($script:tdConnected)) {
 
             $PSCmdlet.ThrowTerminatingError([System.Management.Automation.ErrorRecord]::new("Cannot use TopDesk Client when disconnected.", $null, [System.Management.Automation.ErrorCategory]::InvalidOperation, $null))
 
         }
+
+        $_headerslist = @{
+            'Content-Type' = 'application/json'
+        }
+
+        $_uri = $script:tdURI + '/tas/api/assetmgmt/cardTypes/'
     }
 
     process {
 
-        if ($script:tdConnected) {
-
-            $_uri = $script:tdURI + '/tas/api/assetmgmt/templates'
-            $_all_Templates = Get-APIResponse -Method 'GET' -APIUrl $_uri -Headers @{'Content-Type' = 'application/json'} -tdCredential $script:tdCredential
-
-            if (($null -ne $_all_Templates) -and ($_all_Templates | Get-Member).Name -icontains 'dataSet') {
-
-                foreach ($template in $_all_Templates.dataSet) {
-
-                    #$_returnval.Add($template)
-                    return $template
-
-                }
-
-            }
-            else {
-
-                if ($null -ne $_all_Templates) {
-
-                    Write-Error ("Problem retrieving templates {0}" -f $_all_Templates)
-
-                }
-                else {
-
-                    Write-Error ("Problem retrieving templates")
-
-                }
-            }
-
-        }
-        else {
-
-            Write-Warning ("TopDeskClient has not been connected, try using Connect-TopDesk first.")
-
-        }
+        Get-APIResponse -Method 'GET' -APIUrl $_uri -Headers $_headerslist -tdCredential $script:tdCredential
 
     }
 
