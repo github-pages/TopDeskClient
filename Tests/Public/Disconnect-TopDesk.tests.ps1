e74b8d4aca39047a0bf7b1d752961075bf5069ec
Ray Bury <ray@raybury.com>
2019-04-07 16:48:20 -0400

    Update Tests

--- Tests/Public/Disconnect-TopDesk.tests.ps1
+++ Tests/Public/Disconnect-TopDesk.tests.ps1
@@ -1,5 +1,43 @@
-Describe Disconnect-TopDesk {
-    It "Needs to have real tests" {
-        $true | Should be $true
+Import-Module $PSScriptRoot\..\..\TopDeskClient\TopDeskClient.psd1 -Force
+
+Describe "Disconnect-TopDesk" {
+
+    InModuleScope -ModuleName TopDeskClient {
+
+        $FunctionName = 'Disconnect-TopDesk'
+
+        Context "Disconnecting from tenant" {
+
+            $result = Disconnect-TopDesk
+
+            It "Returns boolean value" {
+                $result | Should -BeOfType boolean
+            }
+
+            It "Disconnects from tenant" {
+                $result | Should -Be $true
+            }
+        }
+
+        Context "Tenant variables after disconnecting" {
+
+            It "Should clear tentant credentials" {
+                $script:tdCredential | Should -BeExactly ([pscredential]::Empty)
+            }
+
+            It "Should clear API Version" {
+                $script:tdAPIVersion | Should -BeExactly 0
+            }
+
+            It "Should clear tdConnected" {
+                $script:tdConnected | Should -BeExactly $false
+            }
+
+            It "Should clear tenant URL" {
+                $script:tdURI | Should -BeNullOrEmpty
+            }
+
+        }
+
     }
 }
