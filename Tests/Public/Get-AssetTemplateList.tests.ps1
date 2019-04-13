0184e3a064ddffac9d05d653ce38b96c63267c51
Ray Bury <ray@raybury.com>
2019-04-12 20:47:15 -0400

    update tests

--- Tests/Public/Get-AssetTemplateList.tests.ps1
+++ Tests/Public/Get-AssetTemplateList.tests.ps1
@@ -1,5 +1,34 @@
+Import-Module $PSScriptRoot\..\..\TopDeskClient\TopDeskClient.psd1 -Force
+
 Describe Get-AssetTemplateList {
-    It "Needs to have real tests" {
-        $true | Should be $true
+
+    InModuleScope -ModuleName TopDeskClient {
+
+        $FunctionName = 'Get-AssetTemplateList'
+
+        Mock -CommandName Get-APIResponse -MockWith { return 'catch-all' }
+
+        Context "Connection state" {
+
+            It "Should throw when client is disconnected" {
+                $script:TDConnected = $false
+                { Get-AssetTemplateList } | Should -Throw
+            }
+        }
+
+        Context "Get AssetTemplateList" {
+
+            $script:TDConnected = $true
+            Mock -CommandName Get-APIResponse -MockWith { return 'AssetTemplateList' } -ParameterFilter { $_uri -and $_uri -eq '/tas/api/assetmgmt/templates' } -Verifiable
+            $AssetTemplateLIst = Get-AssetTemplateList
+
+            It "Calls API to get AssetTemplateList" {
+
+                $AssetTemplateLIst | Should -Not -BeNullOrEmpty
+                $AssetTemplateLIst | Should -Be 'AssetTemplateList'
+                Assert-VerifiableMock
+
+            }
+        }
     }
 }
