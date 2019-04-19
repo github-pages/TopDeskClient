8474c32bfbde0456c2fb2e8b59f2644eae2754bb
Ray Bury <ray@raybury.com>
2019-04-19 11:01:12 -0400

    implement get-person

--- TopDeskClient/Public/Set-Person.ps1
+++ TopDeskClient/Public/Set-Person.ps1
@@ -47,206 +47,253 @@ function Set-Person
         [Parameter(Mandatory = $true,
         ValueFromPipelineByPropertyName = $true)]
         [ValidatePattern('[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}')]
-        [string[]]
+        [string]
         $PersonID,
 
         # LastName of person
         [Parameter(Mandatory = $false,
         ValueFromPipelineByPropertyName = $true)]
         [ValidateLength(1,50)]
-        [string[]]
+        [Alias('LastName')]
+        [string]
         $surName,
 
         # FirstName of person
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateLength(1,30)]
-        [string[]]
+        [Alias('Name','GivenName')]
+        [string]
         $firstName,
 
         # Person initials
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateLength(1,10)]
-        [string[]]
+        [string]
         $firstInitials,
 
         # Person prefix
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateLength(1,10)]
-        [string[]]
+        [string]
         $prefixes,
 
         # Person gender
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateSet('UNDEFINED','MALE','FEMALE')]
-        [string[]]
+        [string]
         $gender,
 
         # Person employee number
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateLength(1,20)]
-        [string[]]
+        [Alias('EmployeeID')]
+        [string]
         $employeeNumber,
 
         # Network login name
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateLength(1,100)]
-        [string[]]
+        [Alias('username')]
+        [string]
         $networkLoginName,
 
         # Branch
-        [Parameter(Mandatory = $false)]
-        [string[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [string]
         $branch,
 
         # Location
-        [Parameter(Mandatory = $false)]
-        [string[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [string]
         $location,
 
         # Department
-        [Parameter(Mandatory = $false)]
-        [string[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [Alias('dept')]
+        [string]
         $department,
 
         # Language
-        [Parameter(Mandatory = $false)]
-        [string[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [Alias('lang')]
+        [string]
         $language,
 
         # Department (only when set to text field)
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateLength(1,60)]
-        [string[]]
+        [string]
         $departmentFree,
 
         # Person login name
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateLength(1,100)]
-        [string[]]
+        [Alias('Login')]
+        [string]
         $tasLoginName,
 
         # Person password (5-200 characters)
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
-        [securestring[]]
+        [securestring]
         $password,
 
         # phone number
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateLength(1,25)]
-        [string[]]
+        [Alias('Phone')]
+        [string]
         $phoneNumber,
 
         # Mobile number
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateLength(1,25)]
-        [string[]]
+        [Alias('cell','mobile')]
+        [string]
         $mobileNumber,
 
         # Fax
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName)]
         [ValidateLength(1,25)]
-        [string[]]
+        [string]
         $fax,
 
         # Email address
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateLength(1,100)]
-        [string[]]
+        [string]
         $email,
 
         # Job Title
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidateLength(1,100)]
-        [string[]]
+        [string]
         $jobTitle,
 
         # View all requests under own budget holder
-        [Parameter(Mandatory = $false)]
-        [bool[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [bool]
         $showBudgetholder,
 
         # View all requests under own department
-        [Parameter(Mandatory = $false)]
-        [bool[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [bool]
         $showDepartment,
 
         # View all requests under own branch
-        [Parameter(Mandatory = $false)]
-        [bool[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [bool]
         $showBranch,
 
         # View all requests under own sub-branch (requires showBranch = $true)
-        [Parameter(Mandatory = $false)]
-        [bool[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [bool]
         $showSubsidiaries,
 
         # Access all branches on forms
-        [Parameter(Mandatory = $false)]
-        [bool[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [bool]
         $showAllBranches,
 
         # Authorize requests for own department/budgetholder/branch/subsidury (requires Manager = $true)
-        [Parameter(Mandatory = $false)]
-        [bool[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [bool]
         $authorizeAll,
 
         # Authorize requests for own department (requires Manager = $true)
-        [Parameter(Mandatory = $false)]
-        [bool[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [bool]
         $authorizeDepartment,
 
         # Authorize requests for own budget holder (requires Manager = $true)
-        [Parameter(Mandatory = $false)]
-        [bool[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [bool]
         $authorizeBudgetHolder,
 
         # Authorize requests for own branch (requires Manager = $true)
-        [Parameter(Mandatory = $false)]
-        [bool[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [bool]
         $authorizeBranch,
 
         # Authorize requests for subsidury locations (requires Manager & Branch = $true)
-        [Parameter(Mandatory = $false)]
-        [bool[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [bool]
         $authorizeSubsidiaryBranches,
 
         # Optional fields 1
-        [Parameter(Mandatory = $false)]
-        [psobject[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [psobject]
         $optionalFields1,
 
         # Optional fields 2
-        [Parameter(Mandatory = $false)]
-        [psobject[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [psobject]
         $optionalFields2,
 
         # Budget Holder
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidatePattern('[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}')]
-        [string[]]
+        [string]
         $budgetHolder,
 
         # Extra field A
-        [Parameter(Mandatory = $false)]
-        [string[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [ValidatePattern('[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}')]
+        [string]
         $personExtraFieldA,
 
         # Extra field B
-        [Parameter(Mandatory = $false)]
-        [string[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [ValidatePattern('[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}')]
+        [string]
         $personExtraFieldB,
 
         # Person is a Manager
-        [Parameter(Mandatory = $false)]
-        [bool[]]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
+        [bool]
         $isManager,
 
         # Manager
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName = $true)]
         [ValidatePattern('[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}')]
-        [string[]]
+        [string]
         $manager
+
     )
 
     begin {
@@ -257,24 +304,32 @@ function Set-Person
 
         }
 
+        $_headerstring = @{
+            'Content-Type' = 'application/json'
+        }
+
     }
     
     process {
 
         foreach ($person in $PersonID) {
-            #$pid = $person
-            $persontoset = [PSCustomObject]@{
-                unid = $person
+
+            if($psboundParameters.Keys.Count -gt 1) {
+
+                $_uri = $script:TDURI + '/tas/api/persons/id/' + $person
+                $persontoset = [PSCustomObject]@{}
+
+                foreach ($item in $psboundParameters.GetEnumerator()) {
+
+                    if($item.key -eq 'PersonID') { continue }
+                    if($null -ne $item.value) {
+                        $persontoset | Add-Member -MemberType NoteProperty -Name $item.key -Value $item.value
+                    }
+                }
+
+                $_body = ($persontoset | ConvertTo-Json)
+                Get-APIResponse -Method 'PUT' -APIUrl $_uri -Body $_body -Headers $_headerstring -tdCredential $script:tdCredential;
             }
-            for ($i = 0; $i -lt $psboundParameters.Count; $i++) {
-                if($psboundParameters[$i] -eq 'PersonID') { continue }
-
-                $persontoset.$psboundParameters[$i].key = $psboundParameters[$i].value
-
-            }
-
-            Write-Output ("person: {0}" -f $persontoset)
-            Write-Output ("Person-json: {0}" -f $persontoset)
         }
     }
     
