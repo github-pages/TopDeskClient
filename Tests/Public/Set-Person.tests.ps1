ac208393b3b715b76151beadddf161be862dae12
Ray Bury <ray@raybury.com>
2019-04-18 21:03:38 -0400

    Add connection state test

--- Tests/Public/Set-Person.tests.ps1
+++ Tests/Public/Set-Person.tests.ps1
@@ -1,61 +1,23 @@
-function Set-Person
-{
-  <#
-    .Synopsis
+Import-Module $PSScriptRoot\..\..\TopDeskClient\TopDeskClient.psd1 -Force
 
-      Short description
+Describe "Set-Person" {
 
-    .DESCRIPTION
+    InModuleScope -ModuleName TopDeskClient {
 
-      Long description
+        $FunctionName = 'Set-Assignment'
 
-    .PARAMETER <Parameter-Name>
+        Mock -CommandName  Get-APIResponse -MockWith { return 'catch-all' }
 
-      The description of a parameter.
+        Context "Connection state" {
 
-    .EXAMPLE
+            It "Should throw when client is disconnected" {
+                $script:TDConnected = $false
+                { Set-Person } | Should -Throw
+            }
 
-      Example of how to use this cmdlet
-
-    .INPUTS
-
-      The Microsoft .NET Framework types of objects that can be piped to the function or script.
-      You can also include a description of the input objects.
-
-    .OUTPUTS
-
-      The .NET Framework type of the objects that the cmdlet returns.
-      You can also include a description of the returned objects.
-
-    .NOTES
-
-      Additional information about the function or script.
-
-    .LINK
-    
-      The name of a related topic. The value appears on the line below the ".LINK" keyword and must be preceded by a comment symbol # or included in the comment block.
-      Repeat the ".LINK" keyword for each related topic.
-  #>
-    [CmdletBinding(DefaultParameterSetName='Default',
-                SupportsShouldProcess=$true,
-                PositionalBinding=$false,
-                HelpUri = 'https://github.com/rbury/',
-                ConfirmImpact='Medium')]
-    [OutputType([String], ParameterSetName = "Default")]
-    Param
-    (
-
-    )
-    begin
-    {
-
-    }
-    process
-    {
-
-    }
-    end
-    {
-
+            It "Should NOT have performed any API calls" {
+                Assert-MockCalled -CommandName Get-APIResponse -Times 0
+            }
+        }
     }
 }
