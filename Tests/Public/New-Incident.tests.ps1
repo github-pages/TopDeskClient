1c14c3dfdd19d5e5a1abf7d1d7c6ad00358f5dc0
Ray Bury <ray@raybury.com>
2019-05-02 08:01:33 -0400

    Update param tests

--- Tests/Public/New-Incident.tests.ps1
+++ Tests/Public/New-Incident.tests.ps1
@@ -38,13 +38,86 @@ Describe "New-Incident" {
 
                 $param = (Get-Command $FunctionName).Parameters['callerLookupEmail']
 
-                It "Should maintain compatiblity" {
+                It "Should maintain compatibility" {
                     $param.ParameterSets.Keys | Should -Contain 'byEmail'
                     $param.ParameterSets.byEmail.IsMandatory | Should -Be $true
                     $param.ParameterSets.byEmail.ValueFromPipelineByPropertyName | Should -Be $false
                     $param.ParameterType.Name | Should -Be 'string'
                 }
             }
+
+            Context "callerLookupID" {
+
+                $param = (Get-Command $FunctionName).Parameters['callerLookupID']
+
+                It "Should maintain compatibility" {
+                    $param.ParameterSets.Keys | Should -Contain 'byID'
+                    $param.ParameterSets.byID.IsMandatory | Should -Be $true
+                    $param.ParameterSets.byID.ValueFromPipelineByPropertyName | Should -Be $false
+                    $param.ParameterType.Name | Should -Be 'string'
+                }
+            }
+
+            Context "callerLookupEmployeeNumber" {
+
+                $param = (Get-Command $FunctionName).Parameters['callerLookupEmployeeNumber']
+
+                It "Should maintain compatibility" {
+                    $param.ParameterSets.Keys | Should -Contain 'byEmployee'
+                    $param.ParameterSets.byEmployee.IsMandatory | Should -Be $true
+                    $param.ParameterSets.byEmployee.ValueFromPipelineByPropertyName | Should -Be $false
+                    $param.ParameterType.Name | Should -Be 'string'
+                }
+            }
+
+            Context "callerLookupNetworkLoginName" {
+
+                $param = (Get-Command $FunctionName).Parameters['callerLookupNetworkLoginName']
+
+                It "Should maintain compatibility" {
+                    $param.ParameterSets.Keys | Should -Contain 'byNetwork'
+                    $param.ParameterSets.byNetwork.IsMandatory | Should -Be $true
+                    $param.ParameterSets.byNetwork.ValueFromPipelineByPropertyName | Should -Be $false
+                    $param.ParameterType.Name | Should -Be 'string'
+                }
+            }
+
+            Context "callerLookupLoginName" {
+
+                $param = (Get-Command $FunctionName).Parameters['callerLookupLoginName']
+
+                It "Should maintain compatibility" {
+                    $param.ParameterSets.Keys | Should -Contain 'byLogin'
+                    $param.ParameterSets.byLogin.IsMandatory | Should -Be $true
+                    $param.ParameterSets.byLogin.ValueFromPipelineByPropertyName | Should -Be $false
+                    $param.ParameterType.Name | Should -Be 'string'
+                }
+            }
+
+            Context "dynamicName" {
+
+                $param = (Get-Command $FunctionName).Parameters['dynamicName']
+
+                It "Should maintain compatibility" {
+                    $param.ParameterSets.Keys | Should -Contain 'Unregistered'
+                    $param.ParameterSets.Unregistered.IsMandatory | Should -Be $true
+                    $param.ParameterSets.Unregistered.ValueFromPipelineByPropertyName | Should -Be $false
+                    $param.ParameterType.Name | Should -Be 'string'
+                }
+            }
+
+            Context "branchID" {
+
+                $param = (Get-Command $FunctionName).Parameters['branchID']
+
+                It "Should maintain compatibility" {
+                    $param.ParameterSets.Keys | Should -Contain 'Unregistered'
+                    $param.ParameterSets.Unregistered.IsMandatory | Should -Be $false
+                    $param.ParameterSets.Unregistered.ValueFromPipelineByPropertyName | Should -Be $false
+                    $param.ParameterType.Name | Should -Be 'string'
+                }
+            }
+
         }
     }
 }
