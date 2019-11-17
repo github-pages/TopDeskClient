c20f94cd87d95ca4a20b83353fbfcc7c162db963
Ray Bury <ray@raybury.com>
2019-11-17 15:07:27 -0500

    Modify fullbuild to remove redundant test

--- TopDeskClient.build.ps1
+++ TopDeskClient.build.ps1
@@ -147,6 +147,6 @@ task Archive {
 
 task regularBuild Clean, PreTest, Build, Analyze, Test
 
-task fullBuild Clean, PreTest, Build, Analyze, Test, GenDocs, Archive
+task fullBuild Clean, Build, Analyze, Test, GenDocs, Archive
 
 task Testing Clean, PreTest, Analyze
